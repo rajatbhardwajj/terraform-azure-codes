705db9e81855b9b3168420d517b3e8404a1d1905
Rajat Bhardwaj <158158803+rajatbhardwajj@users.noreply.github.com>
2024-10-21 13:24:08 +0530

    Create datadisk.tf

---
+++ examples/datadisk.tf
@@ -0,0 +1,15 @@
+resource "azurerm_managed_disk" "disk" {
+    name = "mydatadisk"
+    location = azurerm_linux_virtual_machine.example.location
+    resource_group_name = azurerm_resource_group.example.name
+    storage_account_type = "Standard_LRS"
+    create_option = "Empty"
+    disk_size_gb = 50
+  
+}
+resource "azurerm_virtual_machine_data_disk_attachment" "diskattch" {
+    managed_disk_id = azurerm_managed_disk.disk.id
+    virtual_machine_id = azurerm_linux_virtual_machine.example.id
+    lun = 10
+    caching = "ReadWrite"
+}
